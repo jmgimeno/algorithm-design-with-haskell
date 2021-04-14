bc031f45885e0a7d763ac974180dcfa91548727c
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2021-04-14 18:19:57 +0200

    solved some exercices from chapter1

--- haskell/src/Chapter1.hs
+++ haskell/src/Chapter1.hs
@@ -1,4 +1,7 @@
-module Chapter1 where
+module Chapter1 
+    ( ex1_6
+    , ex1_6'
+    )where
 
 head' :: [a] -> a
 head' = foldr const (error "head of empty")
@@ -72,3 +75,67 @@ fun f e = foldr f e . concat
 
 fun' :: (a -> b -> b) -> b -> [[a]] -> b
 fun' f e = foldr (flip (foldr f)) e
+
+-- Exercise 1.2
+
+uncons :: [a] -> Maybe (a, [a])
+uncons [] = Nothing 
+uncons (x : xs) = Just (x, xs)
+
+-- Exercise 1.3
+
+wrap :: a -> [a]
+wrap x = [x]
+
+unwrap :: [a] -> a
+unwrap [x] = x
+
+single :: [a] -> Bool
+single [_] = True
+single _   = False
+
+-- Exercise 1.4
+
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+-- Exercise 1.5
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+-- Exercise 1.6
+
+{-
+    foldr f e . filter p as an instance of filter
+
+        - h = foldr f e
+
+        - h(filter p xs) = h(foldr (\x acc -> if p x then x : acc else acc) [] xs)
+
+            f x y = if p x then x : y else y
+
+            h(f x y) = g x (h y)  
+
+            h(if p x then x : y else y) = g x (h y) 
+
+        - p x => h (x : y) = g x (h y)
+                 h (x : y) = foldr f e (x : y) 
+                           = f x (foldr f e y) 
+                           = f x (h y)
+                           = g x (h y)
+                           => g = f = \x hy = f x hy
+
+        - otherwise => h y = g x (h y) => g = \ _ hy -> hy
+
+        => g x y = if p x then f x y else y
+-}
+
+ex1_6 :: (a -> Bool) -> (a -> b -> b) -> b -> [a] -> b
+ex1_6 p f e = foldr f e . filter p
+
+ex1_6' :: (a -> Bool) -> (a -> b -> b) -> b -> [a] -> b
+ex1_6' p f = foldr (\x hy -> if p x then f x hy else hy)
--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -1,2 +1,9 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Chapter1 (ex1_6, ex1_6')
+
+import Test.QuickCheck
+import Test.QuickCheck.Function
+
+prop_ex16 :: Eq b => Fun a Bool -> Fun (a, b) b -> b -> [a] -> Bool
+prop_ex16 (Fn p) (Fn2 f) e xs = ex1_6 p f e xs == ex1_6' p f e xs
+
+main = quickCheck (prop_ex16 :: Fun Int Bool -> Fun (Int, Int) Int -> Int -> [Int] -> Bool)
